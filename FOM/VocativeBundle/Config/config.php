4b21f2ee1f25125e72728bc9314610b758f4e33c
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-02-18 14:58:54 +0100

    Skeleton

---
+++ FOM/VocativeBundle/Config/config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'name' => 'First name to vocative',
+    'description' => 'Modifier to convert a first name to its vocative form, useful for email opening salutation.',
+    'author' => 'Friends of Mautic',
+    'version' => '1.0.0',
+
+    'services' => [
+        'events' => [
+            'plugin.vocative.vocativebundle.subscriber' => [
+                'class' => 'MauticPlugin\VocativeBundle\EventListener\VocativeSubscriber'
+            ]
+        ],
+    ],
+];
---
+++ FOM/VocativeBundle/VocativeBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace MauticPlugin\VocativeBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class VocativeBundle extends PluginBundleBase
+{
+
+}
