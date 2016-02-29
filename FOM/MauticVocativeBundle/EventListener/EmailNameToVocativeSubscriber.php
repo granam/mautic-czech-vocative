6a835b5448583738942988ab8f00bccb955b8d6d
jaroslavtyc <mail@jaroslavtyc.com>
2016-02-29 15:12:27 +0100

    fix: Also names from leadfields are vocalized

--- FOM/MauticVocativeBundle/EventListener/EmailNameToVocativeSubscriber.php
+++ FOM/MauticVocativeBundle/EventListener/EmailNameToVocativeSubscriber.php
@@ -15,27 +15,43 @@ class EmailNameToVocativeSubscriber extends CommonSubscriber
     static public function getSubscribedEvents()
     {
         return [
-            EmailEvents::EMAIL_ON_SEND => ['onEmailGenerate', 0],
-            EmailEvents::EMAIL_ON_DISPLAY => ['onEmailGenerate', 0],
+            EmailEvents::EMAIL_ON_SEND => ['onEmailGenerate', -999 /* lowest priority */],
+            EmailEvents::EMAIL_ON_DISPLAY => ['onEmailGenerate', -999 /* lowest priority */],
         ];
     }
 
     public function onEmailGenerate(EmailSendEvent $event)
     {
-        $event->setContent($this->findAndReplace((array)$event->getContent()));
+        $event->setContent($this->findAndReplace((array)$event->getContent(true /* with tokens replaced -  to get names */)));
     }
 
     private function findAndReplace(array $values)
     {
         $replaced = array_map(
             function ($value) {
-                if (preg_match_all('~(?<toReplace>\[(?<toVocative>[^\[\]]+)\|vocative\])~u', $value, $matches) > 0) {
+                /*
+                 * searching for [name|vocative] (enclosed by native or encoded square brackets,
+                 * with name optionally enclosed by [square brackets] as well to match email preview
+                 */
+                if (preg_match_all(
+                        '~(?<toReplace>(?:\[|%5B)(?<prefixToKeep>\[*)(?<toVocative>[^\[\]]+)(?<suffixToKeep>\]*)\|vocative(?:\]|%5D))~u',
+                        $value,
+                        $matches
+                    ) > 0
+                ) {
                     foreach ($matches['toReplace'] as $index => $toReplace) {
                         $toVocative = $matches['toVocative'][$index];
-                        $value = str_replace($toReplace, $this->toVocative($toVocative), $value);
+                        $prefixToKeep = $matches['prefixToKeep'][$index];
+                        $suffixToKeep = $matches['suffixToKeep'][$index];
+                        $value = str_replace($toReplace, $prefixToKeep . $this->toVocative($toVocative) . $suffixToKeep, $value);
                     }
-
-                    return $value;
+                }
+                // lets remove unused '|vocative' modifiers, like empty ones
+                if (preg_match_all('~(?<toRemove>(?:\[|%5B)(?<toKeep>.*)\|vocative(?:\]|%5D))~u', $value, $matches) > 0) {
+                    foreach ($matches['toRemove'] as $index => $toReplace) {
+                        $toKeep = $matches['toKeep'][$index];
+                        $value = str_replace($toReplace, $toKeep, $value);
+                    }
                 }
 
                 return $value;
