869d9cb54a421d729cdfc3bd04d65c4baeea3007
jaroslavtyc <mail@jaroslavtyc.com>
2016-03-06 16:01:38 +0100

    Config part with factory definition tested

--- FOM/MauticVocativeBundle/Tests/Config/ConfigTest.php
+++ FOM/MauticVocativeBundle/Tests/Config/ConfigTest.php
@@ -11,10 +11,20 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         $this->assertFileExists($file = __DIR__ . '/../../Config/config.php');
         $config = include __DIR__ . '/../../Config/config.php';
         $this->assertTrue(is_array($config));
-        array_walk_recursive($config, function($value, $key) {
+        $this->checkClassesExistence($config);
+    }
+
+    private function checkClassesExistence(array $config)
+    {
+        foreach ($config as $key => $value) {
             if ($key === 'class') {
                 $this->assertTrue(class_exists($value));
+            } else if ($key === 'factory') {
+                $this->assertTrue(class_exists($value[0]));
+                $this->assertTrue(is_callable($value[0] . '::' . $value[1]));
+            } else if (is_array($value)) {
+                $this->checkClassesExistence($value);
             }
-        });
+        }
     }
 }
