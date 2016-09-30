4da0428bcdfdbf450d47240811db86b81d93e3c7
Jaroslav Týc <mail@jaroslavtyc.com>
2016-09-30 16:49:04 +0200

    PHP native functions are called in type strict mode if PHP 7+

--- FOM/MauticVocativeBundle/Tests/tests_bootstrap.php
+++ FOM/MauticVocativeBundle/Tests/tests_bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1); // on PHP 7+ are standard PHP methods strict to types of given parameters
+
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 error_reporting(-1);
