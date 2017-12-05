fc0954e40480c05c957179486639973ce08d4e82
Jaroslav Týc <mail@jaroslavtyc.com>
2017-12-05 14:32:47 +0100

    Fix of PHP 7.0 compatibility released

--- FOM/MauticVocativeBundle/Config/config.php
+++ FOM/MauticVocativeBundle/Config/config.php
@@ -3,7 +3,7 @@ return [
     'name' => 'Word to vocative',
     'description' => 'Modifier to convert a name or given gender-dependent alias to its vocative form, useful for email opening salutation.',
     'author' => 'Friends of Mautic',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
 
     'services' => [
         'events' => [
